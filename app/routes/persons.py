03e2cf9b8f0053c9ff79f32cd2d0efd3717e0f0a
Aneesh Kodali <akodali@analytics8.com>
2021-07-27 08:38:46 -0400

    Remove flask forms

--- app/routes/persons.py
+++ app/routes/persons.py
@@ -1,13 +1,9 @@
 # python imports
-from flask import Blueprint, flash, redirect, render_template, url_for
+from flask import Blueprint, render_template
 
 # project imports
 from app import app
-# from ..forms.person_form import PersonForm
 from ..models.persons import PersonModel
-
-home_bp = Blueprint('home_bp', __name__)
-about_bp = Blueprint('about_bp', __name__)
 
 
 
@@ -15,29 +11,3 @@ about_bp = Blueprint('about_bp', __name__)
 def persons():
     persons = PersonModel.query.all()
     return render_template('persons.html', persons=persons)
-
-# @app.route("/persons/new", methods=['GET', 'POST'])
-# def new_person():
-
-    # # generate form
-    # form = PersonForm()
-    
-
-    # # for successful submission
-    # if form.validate_on_submit():
-    #     first_name = form.first_name.data
-    #     last_name = form.last_name.data
-    #     company = form.company.data
-    #     role=form.company.data
-    #     person = PersonModel(first_name=first_name, last_name=last_name, company=company, role=role)
-    #     try:
-    #         person.save_to_db()
-    #         # display success
-    #         person_form_data = f"{form.first_name.data} {form.last_name.data}"
-    #         flash(f"{person_form_data} created!", "success")
-    #         # redirect
-    #         return redirect(url_for('persons'))
-    #     # else return page
-    #     except:
-    #         flash("Unsuccessful, please check data", "danger")
-    # return render_template('persons_new.html', form=form)
