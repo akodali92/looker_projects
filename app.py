5f2317de349bb924b7d33e0e364147bbe265220e
Aneesh Kodali <akodali@analytics8.com>
2021-07-26 22:01:40 -0400

    Create first model

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 # python imports
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from uuid import uuid4
+
+from sqlalchemy.orm import column_property
 
 # project imports
 
 
 # initialize app
 app = Flask(__name__)
+
+# config app
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# create db instance
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+
+    __tablename__ = 'persons'
+
+    # columns
+    sk_person = db.Column(db.String, primary_key=True, default=str(uuid4()).replace('-',''))
+    first_name = db.Column(db.String)
+    last_name = db.Column(db.String)
+    full_name = column_property(first_name + " " + last_name)
+    company = db.Column(db.String)
+    role = db.Column(db.String)
+
+    # print function
+    def __repr__(self):
+        return f"{self.full_name} - {self.company}, {self.role}"
 
 # setup routes
 @app.route("/")
@@ -18,4 +44,4 @@ def about():
 
 # run app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
