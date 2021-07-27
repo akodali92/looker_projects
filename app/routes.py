bb0679567cecd7dfa3792d35b1895d0fbcc67026
Aneesh Kodali <akodali@analytics8.com>
2021-07-26 23:33:15 -0400

    Add route to create new person

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 # python imports
-from flask import redirect, render_template, url_for
+from flask import flash, redirect, render_template, url_for
 
 # project imports
 from app import app
+from .forms.person_form import PersonForm
 from .models.persons import PersonModel
+from app.forms import person_form
 
 
 # setup routes
@@ -17,12 +19,21 @@ def about():
 
 @app.route("/persons")
 def persons():
-
     persons = PersonModel.query.all()
     return render_template('persons.html', persons=persons)
 
-@app.route("/persons/new")
+@app.route("/persons/new", methods=['GET', 'POST'])
 def new_person():
-    person = PersonModel(first_name='Aneesh', last_name='Kodali', company='Analytics8', role='Consultant')
-    person.save_to_db()
-    return redirect(url_for('persons'))
+
+    # generate form
+    form = PersonForm()
+
+    # for successful submission
+    if form.validate_on_submit():
+
+        # display success
+        flash(f"{form.first_name.data} {form.last_name.data} created!")
+        # redirect
+        return redirect(url_for('persons'))
+    # else return page
+    return render_template('persons_new.html', form=form)
