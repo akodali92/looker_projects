a072ef3bf4490d5b029ed0f7115c5557f123735c
Aneesh Kodali <akodali@analytics8.com>
2021-08-02 12:16:06 -0400

    Change sk_date logic

--- app/models/dimensions/dates.py
+++ app/models/dimensions/dates.py
@@ -3,7 +3,6 @@
 # project imports
 from app import db
 from ..shared.base_mixin import BaseMixin
-from ..shared.default_values import default_uuid
 
 
 class DateDimension(BaseMixin, db.Model):
@@ -11,7 +10,7 @@ class DateDimension(BaseMixin, db.Model):
     __tablename__ = 'date_dimension'
 
     # columns
-    sk_date = db.Column(db.String, primary_key=True, default=default_uuid)
+    sk_date = db.Column(db.String, primary_key=True)
     date = db.Column(db.Date, unique=True, nullable=False)
 
     # print function
